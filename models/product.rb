634a8e6c99fb8bb2cf40fb9bb8c52be1a8a366c2
EnigmaticGale <hamishwhytehw@gmail.com>
2019-02-09 20:12:22 +0000

    created the product class with save and delete_all methods, not tested

---
+++ models/product.rb
@@ -0,0 +1,37 @@
+require_relative('../db/sql_runner')
+
+class Product
+
+  attr_accessor :name , :description , :stock , :cost , :sell_price , :supplier_id
+  attr_reader :id
+
+  def initialize(options)
+    @id = options['id'].to_i if options['id']
+    @name = options['name']
+    @description = options['description']
+    @stock = options['stock'].to_i
+    @cost = options['cost'].to_f
+    @sell_price = options['sell_price'].to_f
+    @supplier_id = options['supplier_id'].to_i if options['supplier_id']
+  end
+
+  def save()
+    sql = "INSERT INTO products(name,description,stock,cost,sell_price,supplier_id)
+    VALUES($1, $2, $3, $4, $5, $6)
+    RETURNING id"
+    values = [@name,@description,@stock,@cost,@sell_price,@supplier_id]
+    result = SqlRunner.run(sql, values)
+    id = result.first['id']
+    @id = id
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM products"
+    SqlRunner.run(sql)
+  end
+
+
+
+
+
+end
