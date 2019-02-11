3f33c7ade50ca89477015972bd24fa62b6447e9f
EnigmaticGale <hamishwhytehw@gmail.com>
2019-02-11 10:21:05 +0000

    written untested controllers

---
+++ controller.rb
@@ -0,0 +1,46 @@
+require('sinatra')
+require('sinatra/contrib/all')
+require_relative('models/product')
+require_relative('models/supplier')
+also_reload('./models/*')
+
+get '/stock' do
+  @stock = Product.view_all
+  erb(:stock)
+end
+
+get '/supplier' do
+  @suppliers = Supplier.view_all
+  erb(:suppliers)
+end
+
+get '/product/:id' do
+  @product = Product.find_by_id(params[:id])
+  erb(:product_details)
+end
+
+get 'supplier_of_product/:supplier_id' do
+  @supplier = Supplier.find_by_id
+  erb(:supplier_of_product)
+end
+
+get '/supplier_details/:id' do
+  @supplier = Supplier.find_by_id(params[:id])
+  @products = @supplier.view_products
+  erb(:supplier_details)
+end
+
+get '/edit_product/:id' do
+  @supplier = supplier.view_all
+  @product = Product.find_by_id(params[:id])
+end
+
+post '/new_product' do
+  Product.new(params).save
+  redirect to '/stock'
+end
+
+post '/new_supplier' do
+  Product.new(params).save
+  redirect to '/stock'
+end
