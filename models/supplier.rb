4c0c94a8edfc9421bb8d2db298065cce9f8dfb56
EnigmaticGale <hamishwhytehw@gmail.com>
2019-02-11 09:51:29 +0000

    added assign_to_product and view_products function to supplier class

--- models/supplier.rb
+++ models/supplier.rb
@@ -59,6 +59,19 @@ class Supplier
     return result
   end
 
+  def assign_to_product(product)
+    product.supplier_id = @id
+    product.update
+  end
+
+  def view_products()
+    sql = "SELECT * FROM products WHERE product_id = $1"
+    values = [@id]
+    result = SqlRunner.run(sql, values)
+    suppliers_array = result.map {|supplier| Supplier.new(supplier)}
+    return supplier_array
+  end
+
 
 
 
